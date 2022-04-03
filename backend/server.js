b1bff216e29970224cb22ab94543162f52cba08c
Sk Cus <skcus@hotmail.com>
2022-04-03 19:35:42 +0900

    Video-22-Create-Signin-Backend-API

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,9 @@ import express from 'express';
 import data from './data.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import seedRouter from './rountes/seedRoutes.js';
-import productRouter from './rountes/productRoutes.js';
+import seedRouter from './routes/seedRoutes.js';
+import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -17,8 +18,17 @@ mongoose
   });
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
