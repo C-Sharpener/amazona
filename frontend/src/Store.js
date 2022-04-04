5474eeaaa3a3132f665daeba85ed761126241e30
Sk Cus <skcus@hotmail.com>
2022-04-04 09:39:53 +0900

    Video-24-Create-Shipping-Screen

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -6,7 +6,11 @@ const initialState = {
   userInfo: localStorage.getItem('userInfo')
     ? JSON.parse(localStorage.getItem('userInfo'))
     : null,
+
   cart: {
+    shippingAddress: localStorage.getItem('shippingAddress')
+      ? JSON.parse(localStorage.getItem('shippingAddress'))
+      : {},
     cartItems: localStorage.getItem('cartItems')
       ? JSON.parse(localStorage.getItem('cartItems'))
       : [],
@@ -41,6 +45,17 @@ function reducer(state, action) {
       return {
         ...state,
         userInfo: null,
+        cart: {
+          cartItems: [],
+          shippingAddress: {},
+        },
+      };
+    case 'SAVE_SHIPPING_ADDRESS':
+      return {
+        cart: {
+          ...state.cart,
+          shippingAddress: action.payload,
+        },
       };
     default:
       return state;
